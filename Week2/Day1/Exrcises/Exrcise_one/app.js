3d276cce3bed0372b86da2e05fe8a19507df1275
Avi Asulin <asudiz711@gmail.com>
2021-10-10 14:26:31 +0300

    first commit

---
+++ Week2/Day1/Exrcises/Exrcise_one/app.js
@@ -0,0 +1,26 @@
+    // 5 + "34"
+    // 39
+    // 5 - "4"
+    // 1
+    // 10 % 5
+    // 0
+    // 5 % 10
+    //5
+    //"Java" + "Script"
+    // javascript
+    //" " + " "
+    // space space
+    // " " + 0
+    //  0
+    // true + true
+    // 2
+    // true + false
+    // 1
+    // false + true
+    // 1
+    // false - true
+    // -1
+    // 3 - 4
+    // 1
+    // "Bob" - "bill" 
+    //NaN
