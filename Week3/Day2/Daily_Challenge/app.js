424a860e835ca591264a4dafde46017b43e4ea32
unknown <asudiz711@gmail.com>
2021-10-20 10:00:03 +0300

    Day2_Daily_challenge

--- Week3/Day2/Daily_Challenge/app.js
+++ Week3/Day2/Daily_Challenge/app.js
@@ -1,13 +1,77 @@
-let noun = document.querySelector('#noun');
-let adjective = document.querySelector('#adjective');
-let person = document.querySelector('#person');
-let verb = document.querySelector('#verb');
-let place = document.querySelector('#place');
-let allInputs = document.querySelectorAll('ul');
-let libButton = document.querySelector('#lib-button');
+const fields = document.querySelectorAll("ul>li>input");
+const libBtn = document.querySelector("#lib-button");
+const shufBtn = document.querySelector("#shuffleButton");
+const story = document.querySelector("#story");
+let fieldValues = [];
 
 
-libButton.addEventListener('click', function (e) {
-    console.log(allInputs)
-})
+libBtn.addEventListener("click", libBtnClick);
+shufBtn.addEventListener("click", shuffleValues);
 
+
+function libBtnClick(event) {
+
+    for (input of fields) {
+        if (input.value === "") {
+            alert(`please fill the ${input.id} input`);
+        }
+
+    }
+
+
+    fieldValues = [];
+
+    for (let i = 0; i < fields.length; i++) {
+        fieldValues.push(fields[i].value)
+    }
+
+    resetFields()
+
+    story.innerHTML = fillStory(fieldValues);
+
+}
+
+function shuffleValues() {
+
+    if (fieldValues.length != 5) {
+        alert("First you need to Lib it!")
+        return
+    } else {
+        story.innerHTML = fillStory(shuffle(fieldValues))
+    }
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+
+function resetFields() {
+    for (let i = 0; i < fields.length; i++) {
+        fields[i].value = "";
+    }
+}
+
+
+function fillStory(valuesArr) {
+    const storyArr = ["", " a ", " in ", "", " was arrested this morning ", "", " after", "", " he ", "", "."];
+    const gaps = [0, 3, 5, 7, 9];
+    for (let i = 0; i < 5; i++) {
+        storyArr[gaps[i]] = "<b>" + valuesArr[i] + "</b>";
+    }
+    return storyArr.join("");
+}
