6c1f4267ba99e14700913eeaf37cafbb22b307ef
unknown <asudiz711@gmail.com>
2021-10-21 00:51:20 +0300

    dailr challnge

---
+++ Week3/Day4/Daily_Challenge/app.js
@@ -0,0 +1,46 @@
+let form = document.querySelector('#todoForm');
+let deleteBtn = document.querySelector('#deleteBtn');
+let listDiv = document.querySelector('.listTasks');
+let list = document.createElement('ul');
+
+let listTasks = [];
+
+form.addEventListener('submit', addTask)
+
+function addTask(e) {
+    e.preventDefault()
+
+    let todoInput = e.target.firstElementChild;
+
+    if (todoInput.value === '') {
+        alert('Missing todo')
+    } else {
+
+        listTasks.push(todoInput.value);
+
+        let item = document.createElement('li')
+        let markTodo = document.createElement('input')
+
+
+        markTodo.setAttribute('type', 'checkbox')
+        markTodo.classList.add('deleteInput')
+
+        let itemText = document.createTextNode(todoInput.value)
+
+        listDiv.appendChild(list)
+        item.appendChild(itemText)
+        list.appendChild(item)
+        item.appendChild(markTodo)
+        markTodo.append(icon)
+        markTodo.addEventListener('change', deleteTask)
+
+        todoInput.value = '';
+    }
+
+}
+
+function deleteTask(e) {
+    if (e.target) {
+        e.target.parentElement.remove()
+    }
+}
